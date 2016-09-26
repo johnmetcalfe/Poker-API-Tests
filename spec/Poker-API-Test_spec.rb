b16758ab7013fae33fe0abc1e56242391272215a
johnmetcalfe <john.metcalfe@skybettingandgaming.com>
2016-09-26 12:07:37 +0100

    Few template tests set up

---
+++ spec/Poker-API-Test_spec.rb
@@ -0,0 +1,21 @@
+describe "Api Tests" do
+
+  it "Send winning hand to the API" do
+
+  end
+
+  it "Send losing hand to the API" do
+
+  end
+
+  it "Check cards are shuffled multiple hands" do
+
+  end
+
+  it "Check Royal flush beats straight flush" do
+
+  end
+
+
+
+end
